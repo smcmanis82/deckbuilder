f3396ba8fd13b030db2f83620896917809331c0b
Sharon McManis <sharon.mcmanis@gmail.com>
2021-05-20 14:05:07 -0500

    finished parts two and three

--- src/index.js
+++ src/index.js
@@ -2,23 +2,68 @@
 
 import React, { useState } from "react"; // this enables JSX
 import ReactDOM from "react-dom"; // this allows us to attach the APP
-
+import "./index.css";
 import axios from "axios";
 
 import { DeckList, SearchBar, SearchResults } from "./components";
+
 import { fetchCards } from "./api";
-// import React, from "react";
 
 const App = () => {
   const [results, setResults] = useState([]);
+  const [deck, setDeck] = useState([]);
 
-  fetchCards().then(console.log);
+  const addCardToDeck = ({ id, name }) => {
+    const nextDeck = [...deck]; // make a duplicate first
+    const index = nextDeck.findIndex((card) => card.id === id);
+
+    // index will be -1 if it is not found
+    if (index > -1) {
+      nextDeck[index].count += 1;
+    } else {
+      nextDeck.push({
+        id,
+        name,
+        count: 1,
+      });
+    }
+
+    setDeck(nextDeck);
+  };
+
+  const removeCardFromDeck = ({ id }) => {
+    const nextDeck = [...deck];
+    const index = nextDeck.findIndex((card) => card.id === id);
+
+    if (index === -1) {
+      // don't do anything if we're trying to remove a card not in the deck
+      return;
+    }
+
+    if (nextDeck[index].count === 1) {
+      // remove the card altogether
+      nextDeck.splice(index, 1);
+    } else {
+      // decrement the count
+      nextDeck[index].count -= 1;
+    }
+
+    setDeck(nextDeck);
+  };
 
   return (
     <div id="app">
       <SearchBar setResults={setResults} />
-      <SearchResults results={results} />
-      <DeckList />
+      <SearchResults
+        results={results}
+        addCardToDeck={addCardToDeck}
+        removeCardFromDeck={removeCardFromDeck}
+      />
+      <DeckList
+        deck={deck}
+        addCardToDeck={addCardToDeck}
+        removeCardFromDeck={removeCardFromDeck}
+      />
     </div>
   );
 };
